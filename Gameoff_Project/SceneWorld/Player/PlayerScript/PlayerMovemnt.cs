aa14ca51f69ef0c3aa73799f971d9b412b57d4c0
SgSadguy <tusspukk481@gmail.com>
2023-11-09 14:25:28 +0700

    Add Dash

--- Gameoff_Project/SceneWorld/Player/PlayerScript/PlayerMovemnt.cs
+++ Gameoff_Project/SceneWorld/Player/PlayerScript/PlayerMovemnt.cs
@@ -4,10 +4,9 @@ using System;
 public partial class PlayerMovemnt : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	public const float DashSpeed = 5000.0f;
+	public const bool CanDashSpeed = true;
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -15,18 +14,28 @@ public partial class PlayerMovemnt : CharacterBody2D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
+		if (direction != Vector2.Zero){
 			velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
 		}
-		else
-		{
+		else{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		// Dash Function
+		if (Input.IsActionJustPressed("Dash")){
+			velocity.X = direction.X * DashSpeed;
+			velocity.Y = direction.Y * DashSpeed;
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
 }
+
+
+
+
+
+
