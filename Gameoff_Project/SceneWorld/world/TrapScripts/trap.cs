0b9f0eb988b409e34edb084642a5a1c4ef485064
minoplhy <c@3qx.nl>
2023-11-24 19:19:55 +0700

    what?

--- Gameoff_Project/SceneWorld/world/TrapScripts/trap.cs
+++ Gameoff_Project/SceneWorld/world/TrapScripts/trap.cs
@@ -8,16 +8,11 @@ public partial class Trap : Area2D
 	{
 		
 	}
-	private void _on_area_entered(Area2D area)
+	private void _on_body_entered(Node2D body)
 	{
-		if (area == null) {
-			return;
-			}
-		else {
 			 // Create an instance of MyScript1 and call its function
 			Playerhealth PlayerStandInstance = new Playerhealth();
 			PlayerStandInstance.TakeDamage(10000000);
 			GD.Print("Player has been hitted by a Trap!");
-			}
 	}
 }
